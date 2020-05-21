09bcac41f9f7f792f771fdf0a61892522da744e8
Nstampfli <nathan.stampfli@hetic.net>
2020-05-21 18:33:39 +0200

    add point in carrou and opti function

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -2,12 +2,12 @@ const $carouElement = document.querySelectorAll('.work__carouselElement');
 const $carouElements = document.querySelector('.work__carouselElements');
 const $next = document.querySelector('.work__nextArrow');
 const $prev = document.querySelector('.work__prevArrow');
+const $carouPoint = document.querySelectorAll('.work__carouPoint')
 
 let carouWidth = $carouElements.offsetWidth;
 let elementWidth = carouWidth / $carouElement.length;
 let maxTranslate = carouWidth - elementWidth;
 let shift = 0;
-
 $next.addEventListener('click', function() {
   carouselFunction(1);
 });
@@ -18,16 +18,34 @@ $prev.addEventListener('click', function() {
 
 function carouselFunction(coeff) {
   shift += elementWidth  * coeff;
+  carouPointFunction() 
+  carouConditionFunction()
+  $carouElements.style.transform = 'translateX(-' + shift + 'px)';  
+}
+
+
+function carouPointFunction() {
+  let index  = shift/elementWidth;
+  let indexMax = maxTranslate/elementWidth
+  $carouPoint.forEach(point => {
+    point.classList.remove('activate')
+  });
+  if(index >= 0 && index <= indexMax){
+  $carouPoint[index].classList.add('activate')
+  }
+}
+
+
+function carouConditionFunction() {
   if (shift >= maxTranslate) {
     shift = maxTranslate;
-    $next.style.opacity=0.3;
+    $next.style.visibility='hidden';
   } else if (shift <= 0) {
     shift = 0;
-    $prev.style.opacity=0.3;
+    $prev.style.visibility='hidden';
   }
   else{
-    $prev.style.opacity=1;
-    $next.style.opacity=1;
+    $prev.style.visibility='visible';
+    $next.style.visibility='visible';
   }
-  $carouElements.style.transform = 'translateX(-' + shift + 'px)';
 }
